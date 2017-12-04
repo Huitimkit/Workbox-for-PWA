02b29f3561b835149559cb34b63e4a4a3da0e81c
Huitimkit <350625582@qq.com>
2017-12-04 16:35:15 +0800

    Update sw.js

--- src/sw.js
+++ src/sw.js
@@ -1,7 +1,5 @@
-// TODO: Replace Xs.
 importScripts('/node_modules/workbox-sw/build/importScripts/workbox-sw.prod.v2.1.2.js');
 
-// Note: Ignore the error that Glitch raises about WorkboxSW being undefined.
 const workbox = new WorkboxSW({
   skipWaiting: true,
   clientsClaim: true
